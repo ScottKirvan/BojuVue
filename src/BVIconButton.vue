6949ef6e4ed09aa18ec6935af25778921edf76cb
scott kirvan <fork@skvfx.com>
2026-09-06 19:13:39 -0400

    fix: tweak the size and position of the icon on the BVIconButton

--- src/BVIconButton.vue
+++ src/BVIconButton.vue
@@ -100,8 +100,8 @@ defineOptions({ inheritAttrs: false })
 }
 
 .bv-icon-button-icon :deep(svg) {
-  width: 1em;
-  height: 1em;
+  width: 1.4em;
+  height: 1.4em;
 }
 
 /* Icon-only mode's icon sits centered on top of the (visually empty)
@@ -117,7 +117,7 @@ defineOptions({ inheritAttrs: false })
    sized in .bv-button's own em units. */
 .bv-icon-button.has-icon .bv-icon-button-icon {
   top: 50%;
-  transform: translateY(-50%);
+  transform: translate(-10%, calc(-50% - 0.06em));
 }
 
 .bv-icon-button.has-icon.medium .bv-icon-button-icon {
