813e401a15cea443257df1533fbd3e052f11b1dc
scott kirvan <fork@skvfx.com>
2026-09-07 12:36:44 -0400

    chore: add smiley face svg definition for testing purposes - not used

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -23,6 +23,8 @@ const LIGHTBULB_ICON =
   '<svg viewBox="0 0 24 24" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M9 18h6M10 22h4M12 2a6 6 0 0 0-4 10.472c.53.474 1 1.028 1 1.528v1H15v-1c0-.5.47-1.054 1-1.528A6 6 0 0 0 12 2z"/></svg>'
 const HEART_ICON =
   '<svg viewBox="0 0 24 24" width="16" height="16" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M2 9.5a5.5 5.5 0 0 1 9.591-3.676.56.56 0 0 0 .818 0A5.49 5.49 0 0 1 22 9.5c0 2.29-1.5 4-3 5.5l-5.492 5.313a2 2 0 0 1-3 .019L5 15c-1.5-1.5-3-3.2-3-5.5"/></svg>'
+const FACE_SLIGHTLY_SMILING_ICON =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-face-slightly-smiling"><path d="M15 10V9"/><path d="M16.472 15a6 6 0 01-8.943 0"/><path d="M9 10V9"/><circle cx="12" cy="12" r="10"/></svg>'
 
 export default {
   extends: DefaultTheme,
@@ -43,6 +45,12 @@ export default {
               href: 'https://github.com/sponsors/ScottKirvan',
               icon: HEART_ICON
             }),
+            /*
+            h(BojuVue.BVIconButton, {
+              href: 'https://github.com/sponsors/ScottKirvan',
+              icon: FACE_SLIGHTLY_SMILING_ICON
+            }),
+            */
             h(BojuVue.BVButton, {
               text: 'Getting Started',
               href: '/guide/',
